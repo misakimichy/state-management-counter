684ea02870a8a0e82f88381fa0c43930bba2793d
Misaki Koonce <misaki.koonce@gmail.com>
2020-04-12 22:57:49 -0700

    Check the latest state update is bigger or smaller than previous using useRef.

--- src/HookCounter.js
+++ src/HookCounter.js
@@ -1,34 +1,45 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
-const getStateFormLocalStorage = () => {
-    const storage = localStorage.getItem('counterState')
-    if (storage) return JSON.parse(storage)
-    return { count: 0 }
+const storeStateInLocalStorage = count => {
+    localStorage.setItem('counterState', JSON.stringify({ count }))
+    console.log(localStorage)
 }
 
-const storeStateInLocalStorage = (state) => {
-    localStorage.setItem('counterState', JSON.stringify(state))
-    console.log(localStorage)
+const useLocalStorage = (initialState, key) => {
+    const get = () => {
+        const storage = localStorage.getItem(key)
+        if (storage) return JSON.parse(storage)[value]
+        return initialState
+    }
+
+    const [value, setValue] = useState(get())
+
+    useEffect(() => {
+        localStorage.setItem(key, JSON.stringify({ value }))
+    })
+
+    return [value, setValue]
 }
 
 const HookCounter = ({max, step}) => {
     const [count, setCount] = useState(0)
+    const countRef = useRef()
+
+    let message = ''
+    if (countRef.current < count) message = 'Higher'
+    if (countRef.current > count) message = 'Lower'
+    countRef.current = count
+
     const increment = () => {
-        setCount(c => {
-            if (c >= max) return c
-            return c + step
-        })
+        setCount(c => c + 1)
     }
     const decrement = () => setCount(count - 1)
     const reset = () => setCount(0)
-
-    useEffect(() => {
-        document.title = `Counter: ${count}`
-    }, [count])
     
     return (
         <div className="Counter">
-            <h1 style={{textAlign: 'center'}}>Hook Counter</h1>
+            <h1 style={{ textAlign: 'center' }}>Hook Counter</h1>
+            <p style={{ textAlign: 'center' }}>{message}</p>
             <p className="count">{count}</p>
             <section className="controls">
                 <button onClick={increment}>Increment</button>
